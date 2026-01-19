8c344e233b46ca72d5f6f5d44b979a4b5764216a
Jon Sanjuan <jonathan.s.sanjuan@outlook.com>
2026-01-19 13:19:22 -0500

    Initial Push to remote Repo

---
+++ src/main/java/com/jonathansoriano/enterprisedevgroupproject/EnterpriseDevGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jonathansoriano.enterprisedevgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDevGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseDevGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathansoriano/enterprisedevgroupproject/EnterpriseDevGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathansoriano.enterprisedevgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseDevGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
